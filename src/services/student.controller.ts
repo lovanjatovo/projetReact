e0c10cc116c35ae95c50e5365ae59bdc4bcbd1c6
Lovanjatovo <hei.lovanjatovo@gmail.com>
2026-08-14 10:58:21 +0300

    feat(controller): ajout de la methoded getStudent pour prendre un etudiant par son id

--- src/services/student.controller.ts
+++ src/services/student.controller.ts
@@ -11,3 +11,20 @@ export const getListOfStudents = async (req: Request, res: Response) => {
     res.status(500).json({ message: 'Error in fetching students', error });
   }
 };
+
+// Recuperer un etudiant par ID
+export const getStudent = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id, 10);
+
+  try {
+    const result = await pool.query('SELECT * FROM student WHERE id = $1', [id]);
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: 'The student you looked for is not found' });
+    }
+
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: 'Error in fetching student', error });
+  }
+};
